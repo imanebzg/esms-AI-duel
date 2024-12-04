a15c182bdad3b066312d2b00ed56ec9daedd2500
Imane <imanebouzeguella@gmail.com>
2024-12-05 00:34:11 +0100

    Assembled navbar hero sponsors footer

---
+++ ai-duel/src/Components/AboutEvent/AboutEvent.jsx
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import './AboutEvent.css'; 
+
+
+const AboutEvent = () => {
+ 
+  return (
+    <section id="about">
+        <div  id="abouts"> <img src="/img/about.png" id="abouttext" alt="about-event"/></div>
+        
+    </section>
+  );
+
+};
+    
+
+export default AboutEvent;
---
+++ ai-duel/src/Components/Agenda/Agenda.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Agenda.css'; 
+
+
+const Agenda = () => {
+ 
+  return (
+    <section id="agendas">
+    <div id="agendap">
+      <h1>Agenda</h1>
+    </div>
+    </section>
+  );
+
+};
+    
+
+export default Agenda;
